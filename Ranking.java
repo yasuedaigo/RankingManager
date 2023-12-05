38d7bde3dd542afb4389788d5b1685a8346acc71
DESKTOP-EMLK51G\yasue <yasuedaigo77@gmail.com>
2023-12-05 20:58:24 +0900

    コメント追加

--- Ranking.java
+++ Ranking.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Ranking {
     private List<Integer> pointList;
-    private static final String POINT_RANKING_FORMAT = "%d 位: %d";
     private List<Score> point_name_List;
+    private static final String POINT_RANKING_FORMAT = "%d 位: %d";
     private static final String POINT_NAME_RANKING_FORMAT = "%d 位: %s %d";
 
     public Ranking() {
@@ -13,15 +13,18 @@ public class Ranking {
         point_name_List = new ArrayList<>();
     }
 
+    //スコアだけを登録する
     public void EntryScore(int inputScore) {
         pointList.add(inputScore);
     }
 
+    //スコアと名前を登録する
     public void EntryScore(int inputScore, String inputName) {
         Score score = new Score(inputScore, inputName);
         point_name_List.add(score);
     }
 
+    //スコアのみのランキングを出力する
     public void printPointRanking() {
         // スコアを降順にソート
         Collections.sort(pointList, Collections.reverseOrder());
@@ -32,14 +35,15 @@ public class Ranking {
         }
     }
 
+    //スコアと名前のランキングを出力する
     public void printPoint_NameRanking() {
         // Score オブジェクトのリストをスコアに基づいて降順にソート
-    Collections.sort(point_name_List, (s1, s2) -> Integer.compare(s2.getPoint(), s1.getPoint()));
+        Collections.sort(point_name_List, (s1, s2) -> Integer.compare(s2.getPoint(), s1.getPoint()));
 
-    // 名前とスコアのペアのランキングを出力
-    for (int i = 0; i < point_name_List.size(); i++) {
-        Score score = point_name_List.get(i);
-        System.out.println(String.format(POINT_NAME_RANKING_FORMAT, i + 1, score.getName(), score.getPoint()));
-    }
+        // 名前とスコアのペアのランキングを出力
+        for (int i = 0; i < point_name_List.size(); i++) {
+            Score score = point_name_List.get(i);
+            System.out.println(String.format(POINT_NAME_RANKING_FORMAT, i + 1, score.getName(), score.getPoint()));
+        }
     }
 }
--- RankingManager.java
+++ RankingManager.java
@@ -1,12 +1,14 @@
 public class RankingManager {
     public static void main(String[] args) {
         Ranking ranking = new Ranking();
+        //課題1
         ranking.EntryScore(300);
         ranking.EntryScore(100);
         ranking.EntryScore(400);
         ranking.EntryScore(200);
         ranking.printPointRanking();
 
+        //課題2
         ranking.EntryScore(300, "name300");
         ranking.EntryScore(100, "name100");
         ranking.EntryScore(400, "name400");
